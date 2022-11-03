553649ef8a65e28281ca6e42d1ac6c09f20a7831
hasan <mdhasanhk.98@gmail.com>
2022-11-04 00:14:04 +0600

    first commit

---
+++ starter/script.js
@@ -0,0 +1,67 @@
+// let js = "Amazing";
+// // if (js === "Amazing") alert("javascript is FUN!");
+// 30 + 20 + 40 - 20;
+// console.log(30 + 20 + 40 - 20);
+
+// let friendName = "Rohim Sheikh";
+// console.log(friendName);
+// console.log(friendName);
+// console.log(friendName);
+
+// let myFirstJob = "Teacher";
+// let mySecondJob = "Programmer";
+// console.log(mySecondJob);
+
+// let numbers = 20;
+// console.log(numbers);
+
+// let javaScript = true;
+// console.log(typeof javaScript);
+// console.log(typeof 20);
+// console.log(typeof "Raju");
+
+// //var/let/const:
+// let age = 50;
+// age = 30;
+// console.log(age);
+
+// const birthday = 1998;
+// birthday = 2000;
+// console.log(birthday);
+
+
+//math operators
+// const now = 2030;
+// const ageNayem = now - 1998;
+// const ageMilon = now - 1996;
+// console.log(ageNayem, ageMilon);
+
+// console.log(ageNayem * 2, ageNayem / 20 ** 3)
+// //2**3 means 2 to the power 3 = 2 * 2 * 2;
+
+// const firstName = "md hasan";
+// const lastName = "khan";
+// console.log(firstName + ' ' + lastName);
+
+// //assignment operator
+// let x = 10 + 5; //15
+// x += 10; //x = x + 10
+// x *= 4; //x = x * 4
+// x++;//x = x + 1
+// x--; // x = x - 1s
+// console.log(x);
+
+// //comparison operators
+// console.log(ageNayem > ageMilon) //<, >, <=, >=
+// console.log(ageNayem <= 32);
+// console.log(now - ageNayem < now - ageMilon);
+
+
+const now = 2030;
+const ageNayem = now - 1998;
+const ageMilon = now - 1996;
+
+console.log(now - 1990 > now - 2018);
+
+const averageAge = (ageNayem + ageMilon) / 2;
+console.log(ageNayem, ageMilon, averageAge)
