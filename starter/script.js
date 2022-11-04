146a96e0a658f938be0fc2ee783d3494286c26e4
hasan <mdhasanhk.98@gmail.com>
2022-11-04 23:57:58 +0600

    falsy value

--- starter/script.js
+++ starter/script.js
@@ -83,7 +83,7 @@ const markHigherBMI = MarkBMI > johnBMI;
 console.log(MarkBMI, johnBMI, markHigherBMI)
 */
 
-const firstName = 'Jonas';
+/*const firstName = 'Jonas';
 const job = 'teacher';
 const birthYear = 1998;
 const year = 2022;
@@ -91,4 +91,130 @@ const year = 2022;
 const jonas = 'I am ' + firstName + ', I am a ' + job + ' my age is ' + (year - birthYear);
 console.log(jonas);
 
-const jonasNew = `I am ${firstName} , I am  ${job} `
+const jonasNew = `I am ${firstName} , I am  ${job} ` */
+
+/*
+console.log('hello \n\
+     multiple \n\
+     lines')
+console.log(`hello
+multiple
+lines`)
+*/
+
+
+/*
+const age = 19;
+
+if (age >= 18) {
+    console.log('You are perfect')
+} else {
+    const yourself = 20 - age;
+    console.log(`you are not perfect ${yourself}`)
+}
+*/
+
+/*
+const number = 50;
+
+if (number >= 33) {
+    console.log('you are passed');
+} else {
+    console.log('you are fail');
+}
+*/
+
+/*
+const birthYear = 2012;
+let century;
+if (birthYear <= 2000) {
+    century = 20;
+} else {
+    century = 21;
+}
+console.log(century);
+*/
+
+/*
+// BMI = mass / height ** 2 = mass / (height * height)
+
+const markWeight = 78;
+const johnWeight = 95;
+
+const markHeight = 1.69;
+const johnHeight = 1.95;
+
+const MarkBMI = markWeight / (markHeight * markHeight);
+const johnBMI = johnWeight / (johnHeight * johnHeight);
+const markHigherBMI = MarkBMI > johnBMI;
+
+console.log(MarkBMI, johnBMI, markHigherBMI);
+
+if (MarkBMI > johnBMI) {
+    console.log(`Marks BMI (${MarkBMI}) is higher than john (${johnBMI})`)
+} else {
+    console.log(`john Bmi (${johnBMI}) is higher than Marks (${MarkBMI})`)
+}
+*/
+
+/*
+//type conversion
+const inputYear = '1995';
+console.log(Number(inputYear) + 18)
+
+//type coercion
+console.log('I am ' + 25 + ' years old');
+console.log('25' - '10' - 3);
+console.log('25' + '10' + 3);
+console.log('25' * 2);
+console.log('25' / 2);
+
+let n = '1' + 1;
+n = n - 1;
+console.log(n)
+*/
+
+/*
+// 5 falsy value: 0, '', undefined, null, NuN
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('jonas'));
+console.log(Boolean({}));
+console.log(Boolean(''));
+
+const money = 0;
+if (money) {
+    console.log("don't spend it all")
+} else {
+    console.log("you should get a job")
+};
+
+let height;
+if (height) {
+    console.log("YAY! Height is defined")
+} else {
+    console.log("Height is undefined")
+}
+*/
+
+/*
+const age = 25;
+if (age === 25) console.log('all are perfect (strict)')
+if (age == 25) console.log('all are perfect(loose)')
+
+const favorite = Number(prompt('What is your favorite number?'));
+console.log(favorite);
+console.log(typeof favorite);
+
+if (favorite === 20) {
+    console.log('wow! it is amazing')
+} else if (favorite === 5) {
+    console.log('5 is a cool number')
+} else {
+    console.log("It's not 20 or 5")
+};
+
+if (favorite !== 20) {
+    console.log('why not 25?')
+}
+*/
